7318e609ab84a945140ee6da86b4f5b74b17e790
IngonPhokam <ingon.phokam@gmail.com>
2025-04-26 09:08:22 +0700

    Add files via upload

---
+++ uber_pickups.py
@@ -0,0 +1,94 @@
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYCtest4')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+#cache_data เพื่อเก็บ
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
+#data 
+st.subheader('Raw data')
+st.write(data)
+
+st.subheader('Number of pickups by hour')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+hist_values
+
+
+st.bar_chart(hist_values)
+
+
+hist_values_1= np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))
+
+hist_values_1
+st.bar_chart(hist_values_1)
+st.subheader('Map of all pickups')
+st.map(data)
+
+#การfilter 17.00 (5โมงเย็น)
+hour_to_filter = 17
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
+
+#การfilter 
+hour_to_filter = 12
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
+
+
+hour_to_filter = st.slider('hour', 0, 23, 17)  
+# min: 0h, max: 23h, default: 17h
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
+
+data
+data['lat']
+
+##
+
+####5
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+
+st.session_state.counter += 1
+
+st.header(f"This page has run {st.session_state.counter} times.")
+st.button("Run it again")
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+if "df" not in st.session_state:
+    st.session_state.df = pd.DataFrame(np.random.randn(20, 2), columns=["x", "y"])
+
+st.header("Choose a datapoint color")
+color = st.color_picker("Color", "#FF0000")
+st.divider()
+st.scatter_chart(st.session_state.df, x="x", y="y", color=color)
